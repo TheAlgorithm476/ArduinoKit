51fe7e2a1b83eb8cbbfbb4180a5419a42b49383f
TheAlgorithm476 <thealgorithm476@gmail.com>
2025-10-31 09:43:37 +0100

    Add interrupts and noInterrupts

---
+++ Sources/ArduinoKit/Functions/Interrupts.swift
@@ -0,0 +1,32 @@
+//
+//  Interrupts.swift
+//  ArduinoKit
+//
+//  Created by Brent Van den Abbeel on 2025-10-31.
+//
+
+import CoreAVR
+
+// TODO: Parity with ArduinoCore?
+/// Arduino Reference: Language/Functions/Interrupts/interrupts
+///
+/// Enables Interrupts. Interrupts allow certain tasks to happen in the background, and are disabled by default.
+/// Interrupts can disrupt the timing of code, and should be handled with care.
+/// If you don't know why you're enabling them, you probably don't need them, and should leave them disabled.
+@inlinable
+@inline(__always)
+func interrupts() {
+    Interrupts.enableInterrupts()
+}
+
+// TODO: Parity with ArduinoCore?
+/// Arduino Reference: Language/Functions/Interrupts/noInterrupts
+///
+/// Disables Interrupts. Interrupts allow certain tasks to happen in the background, and are disabled by default.
+/// Interrupts can disrupt the timing of code, and should be handled with care.
+/// If you don't know why you're enabling them, you probably don't need them, and should leave them disabled.
+@inlinable
+@inline(__always)
+func noInterrupts() {
+    Interrupts.disableInterrupts()
+}
